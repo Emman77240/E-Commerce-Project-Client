837c90618074e06083ba0b7cec8b2e2ab36f57fe
Emman77240 <asiegbuemmanuelekee@gmail.com>
2023-04-04 19:50:57 +0100

    Modified server URL

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://earware-api.onrender.com";
+const BASE_URL = "https://earware-api.onrender.com/api/";
 
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
